1118181fc97f994e6c238499662a02430fb522d1
Joespk <supakritjoe@gmail.com>
2024-02-14 23:24:10 +0700

    add banner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import Banner from "./components/Banner";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,16 @@
+const Banner = () => {
+  return (
+    <div className="container mx-auto max-w-[1920px] bg-[url('./src/assets/Banner.jpg')] bg-center bg-cover flex  justify-around h-[635px] items-center">
+      <div className="flex flex-col justify-center gap-4 items-center">
+        <h1 className="text-5xl font-bold text-white drop-shadow-2xl ">
+          Lorem ipsum dolor sit amet consectetur.
+        </h1>
+        <h2 className="text-3xl font-medium text-white drop-shadow-2xl ">
+          One-stop Platform community for Agents and Operator in Thailand.
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
