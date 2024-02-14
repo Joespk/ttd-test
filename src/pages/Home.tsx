b3bb796928437f2f9af90e8851dd406e5afc82ed
Joespk <supakritjoe@gmail.com>
2024-02-15 00:55:37 +0700

    edit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,10 @@ const Home = () => {
   return (
     <div className="flex flex-col gap-16 ">
       <Banner />
-      <div className="flex items-center justify-center flex-col gap-4 ">
-        <h1 className="font-bold text-[32px] items-center m-0">Lorem ipsum</h1>
+      <div className="flex items-center justify-center flex-col gap-4 mx-auto my-0 ">
+        <h1 className="font-bold text-[32px] items-center flex justify-around ">
+          Lorem ipsum
+        </h1>
         <div className="flex flex-row gap-4">
           {allBlog.map((blog) => (
             <Card key={blog.id} blog={blog} />
