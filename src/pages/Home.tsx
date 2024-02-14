b9ee5edaa6909a298f151fbf98a9af8c37403f95
Joespk <supakritjoe@gmail.com>
2024-02-14 23:40:02 +0700

    add temple home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,12 @@ import Banner from "../components/Banner";
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-16 ">
       <Banner />
+      <div className="flex items-center justify-center flex-col gap-6 ">
+        <h1 className="font-bold text-[32px]">Lorem ipsum</h1>
+        <div></div>
+      </div>
     </div>
   );
 };
