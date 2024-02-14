274527d571ce5d8ed01a274c68e8254b2bfa675a
Joespk <supakritjoe@gmail.com>
2024-02-14 23:32:03 +0700

    add BrowserRouter to main tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import "./App.css";
-import Banner from "./components/Banner";
 import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
