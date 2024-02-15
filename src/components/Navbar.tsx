734eab1d602980bed55b224170852a6dd162a80b
Joespk <supakritjoe@gmail.com>
2024-02-15 17:42:49 +0700

    add ui sign in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,18 +4,21 @@ import logo from "../assets/ttdlogo.svg";
 const Navbar = () => {
   return (
     <nav>
-      <div className="container mx-auto py-4 h-20 flex justify-center">
+      <div className="container mx-auto py-4 h-20 flex justify-center overflow-hidden shadow-2xl drop-shadow-xl my-1 max-w-[1920px]">
         <div className="flex flex-row items-center justify-between gap-[500px] ">
-          <img src={logo} alt="logo" />
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+
           <ul className="flex flex-row gap-5 items-center px-5 uppercase">
             <li>
-              <a href="/" className="text-black">
+              <Link to="/" className="text-black">
                 Home
-              </a>
+              </Link>
             </li>
           </ul>
           <Link to="/signin">
-            <button className="bg-[#2A4B6A] w-[141px] p-[16px] text-white font-semibold rounded-[33px] text-xl drop-shadow-2xl">
+            <button className="bg-[#2A4B6A] w-[141px] px-[16px] py-1 text-white font-semibold rounded-[33px] text-xl drop-shadow-2xl">
               Sign In
             </button>
           </Link>
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,5 +1,165 @@
+import mail from "../assets/logo/mail.svg";
+import lock from "../assets/logo/lock.svg";
+import view from "../assets/logo/view.svg";
+import { useState } from "react";
+
 const Signin = () => {
-  return <div>hellowold</div>;
+  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+  const [password, setPassword] = useState<string>("");
+  const [country, setCountry] = useState<string>("Thailand");
+
+  const handleCountryChange = (event: React.FormEvent<HTMLSelectElement>) => {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    setCountry(selectedValue);
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+  return (
+    <div className="container max-w-[1263px] mx-auto px-[250px] rounded-md overflow-hidden shadow-2xl bg-white pb-3  flex flex-col justify-between items-center  drop-shadow-xl  py-4">
+      <form className="grid grid-cols-3 gap-4 justify-between mb-2">
+        <div className="relative col-start-1 ">
+          <h2 className="text-sm ">Email</h2>
+          <div className="relative ">
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className="w-full px-10 py-1 rounded-md border focus:outline-none focus:ring focus:border-blue-500 placeholder:text-sm"
+            />
+            <div className="absolute inset-y-0 left-0 top-[40%] transform -translate-y-1/2 pl-2">
+              <img src={mail} alt="mail" className="h-6 w-6" />
+            </div>
+          </div>
+          <br />
+        </div>
+        <div className="relative col-start-2 ">
+          <h2 className="text-sm">Password</h2>
+          <div className="relative">
+            <input
+              type={passwordVisible ? "text" : "password"}
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-10 py-1 rounded-md border focus:outline-none focus:ring focus:border-blue-500 placeholder:text-sm"
+            />
+            <div className="absolute inset-y-0 left-0 top-[40%] transform -translate-y-1/2 pl-2">
+              <img src={lock} alt="mail" className="h-6 w-6" />
+            </div>
+            <div className="absolute inset-y-0 left-[85%] top-[40%] transform -translate-y-1/2 pl-2">
+              <img
+                src={view}
+                alt="mail"
+                className="h-6 w-6"
+                onClick={togglePasswordVisibility}
+              />
+            </div>
+          </div>
+          <br />
+        </div>
+        <div className="relative col-start-3">
+          <h2 className="text-sm">Confirmed Password</h2>
+          <div className="relative">
+            <input
+              type={passwordVisible ? "text" : "password"}
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-10 py-1 rounded-md border focus:outline-none focus:ring focus:border-blue-500 placeholder:text-sm"
+            />
+            <div className="absolute inset-y-0 left-0 top-[40%] transform -translate-y-1/2 pl-2">
+              <img src={lock} alt="mail" className="h-6 w-6" />
+            </div>
+            <div className="absolute inset-y-0 left-[85%] top-[40%] transform -translate-y-1/2 pl-2">
+              <img
+                src={view}
+                alt="mail"
+                className="h-6 w-6"
+                onClick={togglePasswordVisibility}
+              />
+            </div>
+          </div>
+          <br />
+        </div>
+        <h1 className="col-span-3 mb-2">Information</h1>
+        <div className="col-">
+          <h2 className="text-sm">Company Name</h2>
+          <input
+            type="text"
+            name="Company Name"
+            placeholder="Enter your Company Name"
+            className="w-full px-4 py-1 rounded-md border focus:outline-none focus:ring focus:border-blue-500 placeholder:text-sm"
+          />
+        </div>
+        <div className="col-start-2">
+          <h2 className="text-sm">Tax ID</h2>
+          <input
+            type="text"
+            name="Tax ID"
+            placeholder="Enter Tax ID"
+            className="w-full px-4 py-1 rounded-md border focus:outline-none focus:ring focus:border-blue-500 placeholder:text-sm"
+          />
+        </div>
+        <div className="col-start-3">
+          <h2 className="text-sm">Full Name</h2>
+          <input
+            type="text"
+            name="Fullname"
+            placeholder="Enter Full name"
+            className="w-full px-4 py-1 rounded-md border focus:outline-none focus:ring focus:border-blue-500 placeholder:text-sm"
+          />
+        </div>
+        <div className="col-span-1">
+          <h2 className="text-sm">Country</h2>
+          <select
+            name="country"
+            id="country"
+            value={country}
+            onChange={handleCountryChange}
+            className="w-full pl-4 pr-16 py-1 rounded-md border focus:outline-none focus:ring focus:border-blue-500 text-sm"
+          >
+            <option value="United States">United States</option>
+            <option value="United Kingdom">United Kingdom</option>
+            <option value="Thailand">Thailand</option>
+          </select>
+        </div>
+        <div className="col-start-2 items-center">
+          <h2 className="text-sm">Phone Number</h2>
+          <select
+            name="country"
+            id="country"
+            value={country}
+            onChange={handleCountryChange}
+            className=" py-1 rounded-md border focus:outline-none focus:ring focus:border-blue-500 text-sm"
+          >
+            <option value="United States">United States</option>
+            <option value="United Kingdom">United Kingdom</option>
+            <option value="Thailand">Thailand</option>
+          </select>
+          <input
+            type="text"
+            name="Tax ID"
+            placeholder="Enter Tax ID"
+            className="w-full px-4 py-1 rounded-md border focus:outline-none focus:ring focus:border-blue-500 placeholder:text-sm"
+          />
+        </div>
+        <div className="col-start-3">
+          <h2 className="text-sm">Website</h2>
+          <input
+            type="text"
+            name="Fullname"
+            placeholder="Enter Full name"
+            className="w-full px-4 py-1 rounded-md border focus:outline-none focus:ring focus:border-blue-500 placeholder:text-sm"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Signin;
